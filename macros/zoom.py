0746524e4f024a0964bc865c6c8a6fe12b5860e7
Alan McCoy <armccoy@gmail.com>
2021-10-27 08:53:15 -0400

    Add display placeholders for blank buttons

--- macros/zoom.py
+++ macros/zoom.py
@@ -9,16 +9,16 @@ app = {
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
         (0x540908, 'Audio', [Keycode.COMMAND, Keycode.SHIFT, Keycode.A]),
-        (0x544408, 'Screen', [Keycode.COMMAND, Keycode.SHIFT, Keycode.S]),
+        (0x002000, 'Share', [Keycode.COMMAND, Keycode.SHIFT, Keycode.S]),
         (0x04541B, 'Video', [Keycode.COMMAND, Keycode.SHIFT, Keycode.V]),
         # 2nd row ----------
         (0x000754, 'Control', [Keycode.CONTROL, Keycode.OPTION, Keycode.COMMAND, Keycode.H]),
-        (0x000000, '', []),
+        (0x000754, 'Chat', [Keycode.COMMAND, Keycode.SHIFT, Keycode.H]),
         (0x000754, 'Leave', [Keycode.COMMAND, Keycode.W]),
         # 3rd row ----------
-        (0x000000, '', []),
-        (0x000000, '', []),
-        (0x000000, '', []),
+        (0x000000, '[=====]', []),
+        (0x000000, '[=====]', []),
+        (0x000000, '[=====]', []),
         # 4th row ----------
         (0x200000, 'Mute', [[ConsumerControlCode.MUTE]]),
         (0x080F54, 'Vol-', [[ConsumerControlCode.VOLUME_DECREMENT]]),
