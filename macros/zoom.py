da8690edb8e1e96810e22366073d53719c43dd98
Alan McCoy <armccoy@gmail.com>
2021-11-11 14:48:10 -0500

    Edit zoom key positions

--- macros/zoom.py
+++ macros/zoom.py
@@ -9,11 +9,11 @@ app = {
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
         (0x540908, 'Audio', [Keycode.COMMAND, Keycode.SHIFT, Keycode.A]),
-        (0x002000, 'Share', [Keycode.COMMAND, Keycode.SHIFT, Keycode.S]),
+        (0x000754, 'Chat', [Keycode.COMMAND, Keycode.SHIFT, Keycode.H]),
         (0x04541B, 'Video', [Keycode.COMMAND, Keycode.SHIFT, Keycode.V]),
         # 2nd row ----------
         (0x000754, 'Control', [Keycode.CONTROL, Keycode.OPTION, Keycode.COMMAND, Keycode.H]),
-        (0x000754, 'Chat', [Keycode.COMMAND, Keycode.SHIFT, Keycode.H]),
+        (0x002000, 'Share', [Keycode.COMMAND, Keycode.SHIFT, Keycode.S]),
         (0x000754, 'Leave', [Keycode.COMMAND, Keycode.W]),
         # 3rd row ----------
         (0x000000, '[=====]', []),
