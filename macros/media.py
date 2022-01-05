3d2dc3372bc4abc7f4a82bcf92fdc9df01dadb7a
Alan McCoy <armccoy@gmail.com>
2022-01-05 09:41:27 -0500

    Reorder media controls for consitency with other macros

--- macros/media.py
+++ macros/media.py
@@ -16,12 +16,12 @@ app = {
         (keyconfig.LED_BLANK, keyconfig.KEY_BLANK, []),
         (keyconfig.LED_BLANK, keyconfig.KEY_BLANK, []),
         # 3rd row ----------
-        (0x200000, keyconfig.KEY_MUTE, [[ConsumerControlCode.MUTE]]),
-        (0x080F54, keyconfig.KEY_VOL_DOWN, [[ConsumerControlCode.VOLUME_DECREMENT]]),
-        (0x080F54, keyconfig.KEY_VOL_UP, [[ConsumerControlCode.VOLUME_INCREMENT]]),
-        # 4th row ----------
         (0x202000, '<<', [[ConsumerControlCode.SCAN_PREVIOUS_TRACK]]),
         (0x002000, 'Play/Pause', [[ConsumerControlCode.PLAY_PAUSE]]),
         (0x202000, '>>', [[ConsumerControlCode.SCAN_NEXT_TRACK]]),
+        # 4th row ----------
+        (0x200000, keyconfig.KEY_MUTE, [[ConsumerControlCode.MUTE]]),
+        (0x080F54, keyconfig.KEY_VOL_DOWN, [[ConsumerControlCode.VOLUME_DECREMENT]]),
+        (0x080F54, keyconfig.KEY_VOL_UP, [[ConsumerControlCode.VOLUME_INCREMENT]]),
     ]
 }
