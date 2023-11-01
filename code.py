cc026becbc2ccf16b267369458417b62c2de3310
Alan McCoy <armccoy@gmail.com>
2023-11-01 10:37:00 -0400

    Clean up formatting

--- code.py
+++ code.py
@@ -16,11 +16,9 @@ from adafruit_display_text import label
 from adafruit_macropad import MacroPad
 import keyconfig
 
-
 # CONFIGURABLES ------------------------
 
 MACRO_FOLDER = '/macros'
-
 
 # CLASSES AND FUNCTIONS ----------------
 
@@ -93,15 +91,27 @@ group = displayio.Group()
 for key_index in range(12):
     x = key_index % 3
     y = key_index // 3
-    group.append(label.Label(terminalio.FONT, text='', color=0xFFFFFF,
-                             anchored_position=((macropad.display.width - 1) * x / 2,
-                                                macropad.display.height - 1 -
-                                                (3 - y) * 12),
-                             anchor_point=(x / 2, 1.0)))
+    group.append(
+        label.Label(
+            terminalio.FONT,
+            text='',
+            color=0xFFFFFF,
+            anchored_position=(
+                (macropad.display.width - 1) * x / 2,
+                macropad.display.height - 1 - (3 - y) * 12
+            ), anchor_point=(x / 2, 1.0),
+        )
+    )
 group.append(Rect(0, 0, macropad.display.width, 12, fill=0xFFFFFF))
-group.append(label.Label(terminalio.FONT, text='', color=0x000000,
-                         anchored_position=(macropad.display.width//2, -2),
-                         anchor_point=(0.5, 0.0)))
+group.append(
+    label.Label(
+        terminalio.FONT,
+        text='',
+        color=0x000000,
+        anchored_position=(macropad.display.width//2, -2),
+        anchor_point=(0.5, -0.2)
+    )
+)
 macropad.display.show(group)
 
 # Load all the macro key setups from .py files in MACRO_FOLDER
